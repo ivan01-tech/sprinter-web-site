9534a4f235bc377828145c8673460dbd120b69f7
ivan01-tech <ivansilatsa@gmail.com>
2025-05-09 13:32:45 +0100

    add

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -39,7 +39,7 @@ export default async function BlogPostPage(props: {
   return (
     <>
       <BannerSection
-        title="DETAILS BLOG"
+        title={post.title}
         linkLabel="ACCUEIL / BLOG"
         linkHref="/"
         backgroundImage="/images/image_banner.jpg"
